ff9caa0bf696254dca71a970addaeaaf562e260a
portuga <joaopedromateus6@gmail.com>
2022-03-07 14:30:22 -0300

    ex novo

---
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/01: JavaScript - Primeiros passos/Variáveis/script.js
@@ -0,0 +1,10 @@
+const myName = "João Pedro";
+const birthCity = "Auriflama";
+let birthYear = 2002;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "São Paulo"; //erro
+console.log(birthCity);
