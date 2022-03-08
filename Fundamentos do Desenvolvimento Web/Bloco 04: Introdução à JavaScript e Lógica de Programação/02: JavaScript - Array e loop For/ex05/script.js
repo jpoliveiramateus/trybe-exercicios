03c7271724797977ea3840a57529f5aa20a3fe9c
portuga <joaopedromateus6@gmail.com>
2022-03-08 18:39:33 -0300

    ex05

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex05/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex05/script.js
@@ -1,5 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let maior = 0;
+
 for(i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
+    if(numbers[i] > maior){
+        maior = numbers[i];
+    }
 }
+
+console.log(maior);
