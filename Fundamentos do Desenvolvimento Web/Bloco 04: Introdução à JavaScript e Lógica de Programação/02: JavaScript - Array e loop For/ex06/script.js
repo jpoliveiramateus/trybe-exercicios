b640cf1ed570bfd621747c21585ec81bb4f4e38c
portuga <joaopedromateus6@gmail.com>
2022-03-08 19:07:53 -0300

    ex06

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex06/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex06/script.js
@@ -1,1 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+
+for (let i = 0; i < numbers.length; i += 1){
+    if (numbers[i] % 2 !== 0){
+        impar += 1;
+    }
+}
+
+console.log('Existe ' + impar + ' números ímpares dentro da array.');
