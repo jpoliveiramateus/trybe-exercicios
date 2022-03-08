ae2eb95c02e9e81db0f2129523293241ce3fc572
portuga <joaopedromateus6@gmail.com>
2022-03-08 15:56:59 -0300

    ex04

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex04/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/ex04/script.js
@@ -1,1 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let totalElementos = numbers.length;
+
+for (let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+}
+
+let resultado = (soma / totalElementos);
+
+if (resultado > 20){
+    console.log('valor maior que 20.');
+} else {
+    console.log('valor menor ou igual a 20.');
+}
