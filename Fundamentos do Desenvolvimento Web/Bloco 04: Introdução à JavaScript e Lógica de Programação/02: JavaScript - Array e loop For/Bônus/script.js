501807d8f84d07bbd93e8c926c9e07e3276581f3
portuga <joaopedromateus6@gmail.com>
2022-03-09 16:03:03 -0300

    exbonus

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/Bônus/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/02: JavaScript - Array e loop For/Bônus/script.js
@@ -1,7 +1,31 @@
+//Ordenar em ordem crescente
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 0; i < numbers.length; i += 1){
-    for(let ind = numbers[0]; ind < numbers.length - 1; ind += 1){
-        console.log(numbers[ind]);
+    for (let j = 0; j < (numbers.length - i - 1); j += 1){
+        if (numbers[j] > numbers[j + 1]){
+            let tmp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = tmp;
+        }
     }
 }
+
+console.log(numbers);
+
+//Ordenar em ordem decrecente
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 0; i < numbers.length; i += 1){
+//     for (let j = 0; j < (numbers.length - i - 1); j += 1){
+//         if (numbers[j] > numbers[j + 1]){
+//             let tmp = numbers[j];
+//             numbers[j] = numbers[j + 1];
+//             numbers[j + 1] = tmp;
+//         }
+//     }
+// }
+
+// console.log(numbers);
