4a4490e2e36b8dca40409e4b9b8d8e5c34a45de0
portuga <joaopedromateus6@gmail.com>
2022-03-10 23:46:48 -0300

    ex

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex03 - Maior e menor número de palavra dentro do array/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex03 - Maior e menor número de palavra dentro do array/script.js
@@ -1,16 +1,16 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-let maior = array[0].length;
-let menor = array[0].length;
+let maior = array[0];
+let menor = array[0];
 
 for (let i = 0; i < array.length; i += 1){
-    if(array[i].length > maior){
+    if(array[i].length > maior.length){
         maior = array[i];
     }
 }
 
 for (let i = 0; i < array.length; i += 1){
-    if(array[i].length < menor){
+    if(array[i].length < menor.length){
         menor = array[i];
     }
 }
--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
@@ -1,6 +1,3 @@
-let numeros = [];
-
 for (let i = 0; i <= 50; i += 1) {
-    numeros.push(i);
+    
 }
-
