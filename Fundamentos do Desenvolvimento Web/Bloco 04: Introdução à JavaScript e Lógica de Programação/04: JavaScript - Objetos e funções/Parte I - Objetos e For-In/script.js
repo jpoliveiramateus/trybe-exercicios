a5bf4ee5d0d2570058fc0597981ee1ffc1b1a3ce
portuga <joaopedromateus6@gmail.com>
2022-03-10 22:31:05 -0300

    resolvendo

---
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/04: JavaScript - Objetos e funções/Parte I - Objetos e For-In/script.js
@@ -0,0 +1,46 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+// console.log('Bem-vinda, ' + info.personagem);
+
+
+info.recorrente = 'Sim';
+// console.log(info);
+
+// for (let key in info){
+//     console.log(key);
+// }
+
+// for (let key in info){
+//     console.log(info[key]);
+// }
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+
+// console.log(info.personagem + ' e ' + info2.personagem);
+// console.log(info.origem + ' e ' + info2.origem);
+// console.log(info.nota + ' e ' + info2.nota);
+// console.log('Ambos recorrentes');
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos['titulo']);
