d30caa5668949f3056a5cd5edbe79bc8d58f8e5e
portuga <joaopedromateus6@gmail.com>
2022-03-12 19:02:49 -0300

    ex

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
@@ -1,14 +1,4 @@
-let n = 8;
-let count = 0;
-
-for (let i = 0; i <= n; i += 1){
-    if(n % i === 0){
-        count += 1;
-    }
-}
-
-if(count == 2){
-    console.log('número primo');
-} else {
-    console.log('não é primo');
+let numeros = [];
+for (let i = 0; i <= 50; i += 1) {
+    numeros.push(i);
 }
