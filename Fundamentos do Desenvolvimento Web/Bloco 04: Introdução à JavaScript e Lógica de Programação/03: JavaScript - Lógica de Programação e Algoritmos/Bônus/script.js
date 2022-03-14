b8f90446f511b3a1823c86fbf4b23109e46fbd01
portuga <joaopedromateus6@gmail.com>
2022-03-14 19:25:43 -0300

    tentando ex

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/Bônus/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/Bônus/script.js
@@ -1,4 +1,4 @@
-n = 6;
+n = 7;
 /*
 for (let i = n; n >= 1; n -= 1) {
     let asterisco = '*';
@@ -19,15 +19,29 @@ for (let i = n; n >= 1; n -= 1) {
     console.log(espaco.repeat(i - count) + asterisco.repeat(count));
     count += 1;
 }
-*/
-let count = n - 2;
+
+let count = 3;
 let countTwo = 1;
 for (let i = n; n >= 1; n -= 1) {
     let asterisco = '*';
     let espaco = ' ';
-    if (count > 0 || countTwo <= i) {
+    if (count >= 0 && countTwo <= i) {
         console.log(espaco.repeat(count) + asterisco.repeat(countTwo));
     }
     count -= 1;
     countTwo += 2;
+}
+*/
+let count = 3;
+let countTwo = 0;
+let som = 0;
+for (let i = n; n >= 1; n -= 1) {
+    let asterisco = '*';
+    let espaco = ' ';
+    if (count >= 0 && countTwo <= i) {
+        console.log(espaco.repeat(count) + asterisco.repeat(1) + espaco.repeat(countTwo) + asterisco.repeat(countTwo));
+    }
+    count -= 1;
+    countTwo += 1 + som;
+    som += 1;
 }
