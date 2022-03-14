7c9639979111f2f0aa1d703060521402f8378e14
portuga <joaopedromateus6@gmail.com>
2022-03-14 12:32:43 -0300

    exprimos

---
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/Bônus/script.js
@@ -0,0 +1,33 @@
+n = 6;
+/*
+for (let i = n; n >= 1; n -= 1) {
+    let asterisco = '*';
+    console.log(asterisco.repeat(i));
+}
+
+let count = 1;
+for (let i = n; n >= 1; n -= 1) {
+    let asterisco = '*';
+    console.log(asterisco.repeat(count));
+    count += 1;
+}
+
+let count = 1;
+for (let i = n; n >= 1; n -= 1) {
+    let asterisco = '*';
+    let espaco = ' ';
+    console.log(espaco.repeat(i - count) + asterisco.repeat(count));
+    count += 1;
+}
+*/
+let count = n - 2;
+let countTwo = 1;
+for (let i = n; n >= 1; n -= 1) {
+    let asterisco = '*';
+    let espaco = ' ';
+    if (count > 0 || countTwo <= i) {
+        console.log(espaco.repeat(count) + asterisco.repeat(countTwo));
+    }
+    count -= 1;
+    countTwo += 2;
+}
--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/ex04 - Números primos entre 0 e 50/script.js
@@ -1,4 +1,13 @@
-let numeros = [];
-for (let i = 0; i <= 50; i += 1) {
-    numeros.push(i);
+let biggestNumber = 0;
+for (let n = 0; n <= 50; n += 1) {
+    let count = 0;
+    for (let i = 0; i <= n; i += 1) {
+        if (n % i === 0) {
+            count += 1;
+        }
+    }
+    if (count == 2 && n > biggestNumber) {
+        biggestNumber = n;
+    }
 }
+console.log(biggestNumber);
