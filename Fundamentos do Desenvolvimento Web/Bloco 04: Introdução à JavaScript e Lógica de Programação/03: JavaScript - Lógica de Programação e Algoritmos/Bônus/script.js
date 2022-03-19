1872c1e1dd22816fa4e053863f31248547ca676f
portuga <joaopedromateus6@gmail.com>
2022-03-19 13:41:55 -0300

    ex

--- Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/Bônus/script.js
+++ Fundamentos do Desenvolvimento Web/Bloco 04: Introdução à JavaScript e Lógica de Programação/03: JavaScript - Lógica de Programação e Algoritmos/Bônus/script.js
@@ -1,35 +1,30 @@
 let n = 5;
 /*
+// ex01
 for (let i = 1; i <= n; i += 1) {
     console.log('*'.repeat(n));
 }
-
+// ex02
 for (let i = 1; i <= n; i += 1) {
     console.log('*'.repeat(i));
 }
-
+// ex03
 let contador = n - 1;
 for (let i = 1; i <= n; i += 1) {
     console.log(' '.repeat(contador) + '*'.repeat(i));
     contador -= 1;
 }
 */
-let inputLine = '';
+// ex06
+let count = 0;
 
-let midOfMatrix = (n + 1) / 2;
-let right = midOfMatrix;
-let left = midOfMatrix;
-
-for (let i = 0; i <= midOfMatrix; i += 1) {
-    for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
-        if (columnIndex > left && columnIndex < right) {
-          inputLine = inputLine + '*';
-        } else {
-          inputLine = inputLine + ' ';
-        }
+for (let i = 0; i <= n; i += 1) {
+    if (n % i === 0) {
+        count += 1;
     }
-    console.log(inputLine);
-    inputLine = '';
-    right += 1;
-    left -= 1;
-};
+}
+if (count == 2) {
+    console.log('É primo.');
+} else {
+    console.log('Não é primo.');
+}
