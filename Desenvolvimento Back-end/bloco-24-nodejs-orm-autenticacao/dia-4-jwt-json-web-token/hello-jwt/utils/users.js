1bc3049e03fe6d6a2578eec654245cadcb060fbc
jpoliveiramateus <joaopedromateus6@gmail.com>
2022-09-21 19:26:47 -0300

    ex24.4

---
+++ Desenvolvimento Back-end/bloco-24-nodejs-orm-autenticacao/dia-4-jwt-json-web-token/hello-jwt/utils/users.js
@@ -0,0 +1,30 @@
+const fs = require('fs').promises;
+const { join } = require('path');
+
+const writeUserFile = async (content) => {
+  const path = '../models/data/users.json';
+  try {
+    await fs.writeFile(join(__dirname, path), JSON.stringify(content));
+  } catch (error) {
+    return console.error('Erro ao escrever arquivo:', error.path);
+  }
+};
+
+const readUserFile = async () => {
+  const path = '../models/data/users.json';
+  try {
+    const contentFile = await fs.readFile(join(__dirname, path), 'utf-8');
+    return JSON.parse(contentFile);
+  } catch (error) {
+    return console.error('Erro ao ler arquivo:', error.path);
+  }
+};
+
+// const findUser = async (userName) => {
+//   const users = await readUserFile();
+//   const user = users.find(us => us.username === user);
+// };
+
+module.exports = {
+  // findUser,
+};
